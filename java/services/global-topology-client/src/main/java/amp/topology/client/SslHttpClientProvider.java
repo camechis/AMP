9eac065dcca5491ca32f1b961c17483ce233ebbc
Richard Clayton <rclayton@bericotechnologies.com>
2013-04-02 21:30:07 -0400

    Added Truststore configuration options to the Java Global Topology Client.  Fixes #11.

--- java/services/global-topology-client/src/main/java/amp/topology/client/SslHttpClientProvider.java
+++ java/services/global-topology-client/src/main/java/amp/topology/client/SslHttpClientProvider.java
@@ -2,7 +2,11 @@ package amp.topology.client;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.scheme.Scheme;
@@ -24,13 +28,57 @@ public class SslHttpClientProvider implements HttpClientProvider {
 	
 	String keystore;
 	
-	String password;
+	String keystorePassword;
 	
+	String truststore;
+	
+	/**
+	 * It's okay if this is null if you are either not using a trust store,
+	 * or the trust store has no password.
+	 */
+	String truststorePassword = null;
+	
+	/**
+	 * Instantiate using a password for the TrustStore.
+	 * @param keystore Path to Key store
+	 * @param keystorePassword Password of Key store (private key)
+	 * @param port Port of the remote server.
+	 */
 	public SslHttpClientProvider(String keystore, String keystorePassword, int port){
 		
 		this.keystore = keystore;
-		this.password = keystorePassword;
+		this.keystorePassword = keystorePassword;
 		this.port = port;
+	}
+	
+	/**
+	 * Instantiate using a password for the TrustStore.
+	 * @param keystore Path to Key store
+	 * @param keystorePassword Password of Key store (private key)
+	 * @param truststore Path to Trust store
+	 * @param port Port of the remote server.
+	 */
+	public SslHttpClientProvider(String keystore, String keystorePassword, String truststore, int port){
+		
+		this(keystore, keystorePassword, port);
+		
+		this.truststore = truststore;
+	}
+	
+	/**
+	 * Instantiate using a password for the TrustStore.
+	 * @param keystore Path to Key store
+	 * @param keystorePassword Password of Key store (private key)
+	 * @param truststore Path to Trust store
+	 * @param trustStorePassword Trust store password
+	 * @param port Port of the remote server.
+	 */
+	public SslHttpClientProvider(String keystore, String keystorePassword, 
+			String truststore, String trustStorePassword, int port){
+		
+		this(keystore, keystorePassword, truststore, port);
+		
+		this.truststorePassword = trustStorePassword;
 	}
 	
 	@Override
@@ -41,44 +89,63 @@ public class SslHttpClientProvider implements HttpClientProvider {
 
 	/**
 	 * Register a Trust Store with the HttpClient.
-	 * @param httpClient HttpClient to register the trust store.
-	 * @param port Port of the server in which the connection will be made.
-	 * @param keystorePath Path on disk to the keystore.
-	 * @param keystorePassword Password of the keystore.
-	 * @throws Exception If the file can't be pulled from the FS, an exception is thrown.
 	 */
 	public HttpClient createClient() {
 		
-			logger.debug("Creating HttpClient.");
+		logger.debug("Creating HttpClient.");
 		
-			FileInputStream instream = null;
-			SSLSocketFactory socketFactory = null;
+		SSLSocketFactory socketFactory = null;
+		
+		try {
+		
+			KeyStore loadedKeystore = getAndLoad(this.keystore, this.keystorePassword);
 			
-			try {
-			
-				KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			if (this.truststore != null){
 				
-				instream  = new FileInputStream(new File(this.keystore));
+				KeyStore loadedTruststore = getAndLoad(this.truststore, this.truststorePassword);
 				
-				trustStore.load(instream, this.password.toCharArray());
+				socketFactory = new SSLSocketFactory(loadedKeystore, this.keystorePassword, loadedTruststore);
 				
-				socketFactory = new SSLSocketFactory(trustStore, this.password, trustStore);
+			} else {
 				
-			} catch (Exception e){  
-				
-				logger.error("Could not instantiate an SSLSocketFactory: {}", e);
-			}  
-			finally {
-			
-				try { instream.close(); } catch (Exception ignore) {}
+				socketFactory = new SSLSocketFactory(loadedKeystore, this.keystorePassword, loadedKeystore);
 			}
 			
-			Scheme scheme = new Scheme("https", this.port, socketFactory);
+		} catch (Exception e){  
 			
-			DefaultHttpClient httpClient = new DefaultHttpClient();
-			
-			httpClient.getConnectionManager().getSchemeRegistry().register(scheme);
-			
-			return httpClient;
+			logger.error("Could not instantiate an SSLSocketFactory: {}", e);
 		}
+		
+		Scheme scheme = new Scheme("https", this.port, socketFactory);
+		
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		
+		httpClient.getConnectionManager().getSchemeRegistry().register(scheme);
+		
+		return httpClient;
+	}
+
+	/**
+	 * Get an instance of the KeyStore and Load it with Certs using the supplied password.
+	 * @param path Location of the KeyStore
+	 * @param password Password, which can be null for the trust store.
+	 * @return A loaded KeyStore instance.
+	 * @throws KeyStoreException
+	 * @throws NoSuchAlgorithmException 
+	 * @throws CertificateException
+	 * @throws IOException
+	 */
+	static KeyStore getAndLoad(String path, String password) 
+			throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+		
+		char[] charPassword = (password == null)? null : password.toCharArray();
+		
+		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+		
+		FileInputStream fis = new FileInputStream(new File(path));
+		
+		keystore.load(fis, charPassword);
+		
+		return keystore;
+	}
 }
