e1461d4f0870de3d4838d610d107bd25c06267f4
Minh Duc Nguyen <mnguyen@bericotechnologies.com>
2013-04-25 10:35:09 -0400

    AMP-40 - Adding example classes from archetype Fallwizard project

--- java/services/global-event-log/src/main/java/amp/gel/managed/ExampleManaged.java
+++ java/services/global-event-log/src/main/java/amp/gel/managed/ExampleManaged.java
@@ -4,15 +4,13 @@ import com.yammer.dropwizard.lifecycle.Managed;
 
 public class ExampleManaged implements Managed {
 
-	@Override
 	public void start() throws Exception {
-		
+
 		System.out.println("Started.");
 	}
 
-	@Override
 	public void stop() throws Exception {
-		
+
 		System.out.println("Stop.");
 	}
 
