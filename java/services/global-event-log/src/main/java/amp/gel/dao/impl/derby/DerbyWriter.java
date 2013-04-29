60eac12333f88fe67f6c2e0c698328ae4f344b24
Minh Duc Nguyen <mnguyen@bericotechnologies.com>
2013-04-29 18:16:28 -0400

    AMP-41 - Created DatastoreWriter for Apache Derby

---
+++ java/services/global-event-log/src/main/java/amp/gel/dao/impl/derby/DerbyWriter.java
@@ -0,0 +1,31 @@
+package amp.gel.dao.impl.derby;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import amp.gel.dao.DatastoreWriter;
+
+public class DerbyWriter implements DatastoreWriter {
+
+	private EntityManager entityManager;
+
+	@PersistenceContext
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public void initialize() throws Exception {
+		// do nothing
+	}
+
+	@Transactional(readOnly = false)
+	public void write(cmf.bus.Envelope envelope) throws Exception {
+		entityManager.persist(new Envelope(envelope));
+	}
+
+	public void close() {
+		// do nothing
+	}
+}
