405c9b37c6e799eb6a197b55c0929cef2cedf5d9
Jared Holmberg <jholmberg@bericotechnologies.com>
2013-06-04 08:45:47 -0400

    Simplified logic in stream based publish method. Set version to snapshot until ready for release.

--- java/amp-eventing/src/main/java/amp/eventing/DefaultEventBus.java
+++ java/amp-eventing/src/main/java/amp/eventing/DefaultEventBus.java
@@ -18,7 +18,15 @@ public class DefaultEventBus implements IEventBus, IStreamingEventBus, IInboundP
     protected IEnvelopeBus envelopeBus;
     protected List<IEventProcessor> inboundProcessors = new LinkedList<IEventProcessor>();
     protected List<IEventProcessor> outboundProcessors = new LinkedList<IEventProcessor>();
+    /**
+     * Default setting for {@link cmf.eventing.IStreamingEventBus} to stream events in batches.
+     * Allows for tuning by setting this to a different value based on size of events.
+     */
     protected int batchLimit = 10;
+
+    protected static final String SEQUENCE_ID = "sequenceId";
+    protected static final String POSITION = "position";
+    protected static final String IS_LAST = "isLast";
 
     public DefaultEventBus(IEnvelopeBus envelopeBus) {
         this.envelopeBus = envelopeBus;
@@ -96,99 +104,6 @@ public class DefaultEventBus implements IEventBus, IStreamingEventBus, IInboundP
         log.debug("leave publish");
     }
 
-    public static final String SEQUENCE_ID = "sequenceId";
-    public static final String POSITION = "position";
-    public static final String IS_LAST = "isLast";
-
-    @Override
-    public <TEVENT> void publishToStream(Iterator<Object> eventStream,
-                                         IStreamingMapperCallback<TEVENT> objectMapper) throws Exception {
-        log.debug("enter publish to stream");
-
-        if (null == eventStream) {
-            throw new IllegalArgumentException("Cannot publish a null event stream");
-        }
-
-        boolean doMap = true;
-        if (null == objectMapper) {
-            log.warn("No IStreamingMapperCallback supplied. Will not perform mapping of elements in event stream");
-            doMap = false;
-        }
-
-
-        String sequenceId = UUID.randomUUID().toString();
-        int position = 0;
-        boolean isLast;
-        int limitCounter = 0;
-        Queue<EventStreamQueueItem> streamQueue = new LinkedList<EventStreamQueueItem>();
-
-        while (eventStream.hasNext()) {
-            Object eventItem = eventStream.next();
-            isLast = (!eventStream.hasNext()) ?  true : false;
-            if (doMap) {
-                //This may look a little strange, but just reusing eventItem again for efficiency
-                //eventItem is now of type TEVENT which was converted to conform to the sender's desired format
-                //before getting serialized
-                eventItem = objectMapper.map(eventItem);
-            }
-
-
-            if ((limitCounter < this.batchLimit) || false == isLast) {
-                Envelope envelope = buildStreamingEnvelope(sequenceId, position, Boolean.toString(isLast));
-                EventContext context = new EventContext(Directions.Out, envelope, eventItem);
-                streamQueue.add(new EventStreamQueueItem(context));
-                limitCounter++;
-                log.debug("Buffering event to be streamed with sequenceId: " + sequenceId + ", position: " + position + ", isLast: " + isLast);
-            } else {
-                while (streamQueue.size() > 0) {
-                    final EventStreamQueueItem queueItem = streamQueue.remove();
-                    this.processEvent(queueItem.getEventContext(),
-                            outboundProcessors,
-                            new IContinuationCallback() {
-                                @Override
-                                public void continueProcessing() throws Exception {
-                                    envelopeBus.send(queueItem.getEnvelope());
-                                }
-                            });
-                    log.debug("Sending streamed event with sequenceId: " +
-                            queueItem.getEnvelope().getHeader(SEQUENCE_ID) +
-                            ", position: " + queueItem.getEnvelope().getHeader(POSITION) +
-                            ", isLast: " + queueItem.getEnvelope().getHeader(IS_LAST));
-
-                }
-                limitCounter = 0;
-                streamQueue.clear();
-            }
-            position++;
-
-
-        }
-        log.debug("leave publish to stream");
-    }
-
-    /**
-     * Publishes messages on the {@link cmf.eventing.IStreamingEventBus} after the numberOfEvents limit has been met.
-     *
-     * @param numberOfEvents
-     */
-    @Override
-    public void setBatchLimit(int numberOfEvents) {
-        if (numberOfEvents <= 0) {
-            log.warn("Message batch limit cannot be less than zero, using 10 as the default limit.");
-            this.batchLimit = 10;
-        } else {
-            this.batchLimit = numberOfEvents;
-        }
-    }
-
-    protected Envelope buildStreamingEnvelope(String sequenceId, int position, String isLast) {
-        Envelope envelope = new Envelope();
-        envelope.setHeader(SEQUENCE_ID, sequenceId);
-        envelope.setHeader(POSITION, position + "");
-        envelope.setHeader(IS_LAST, isLast);
-        return envelope;
-    }
-
     @Override
     public <TEVENT> void subscribe(IEventHandler<TEVENT> eventHandler) throws Exception {
         Class<TEVENT> type = eventHandler.getEventType();
@@ -227,5 +142,117 @@ public class DefaultEventBus implements IEventBus, IStreamingEventBus, IInboundP
         dispose();
     }
 
+    /**
+     * Iterates on the eventStream and calls the object mapper to convert the object to the desired format before
+     * publishing the event to the bus.
+     * <p>
+     *     In addition 3 new headers will be added to each event that is published:<br />
+     *     <ol>
+     *         <li>sequenceId : A UUID that ties this event to a particular sequence. This is the unique identifier that
+     *         indicates the message is part of a larger data set</li>
+     *         <li>position : An integer that indicates what position in the sequence this is.</li>
+     *         <li>isLast : A boolean indicating if this event is the last message in the sequence</li>
+     *     </ol>
+     * </p>
+     * @param eventStream
+     * @param objectMapper
+     * @param <TEVENT>
+     * @throws Exception
+     */
+    @Override
+    public <TEVENT> void publishStream(Iterator<Object> eventStream,
+                                       IStreamingMapperCallback<TEVENT> objectMapper) throws Exception {
+        log.debug("enter publish to stream");
+
+        validateEventStream(eventStream);
+        boolean doMap = isValidMapper(objectMapper);
+
+        String sequenceId = UUID.randomUUID().toString();
+        int position = 0;
+        boolean isLast;
+        Queue<EventStreamQueueItem> streamBuffer = new LinkedList<EventStreamQueueItem>();
+
+        while (eventStream.hasNext()) {
+            Object eventItem = eventStream.next();
+            isLast = (!eventStream.hasNext()) ? true : false;
+            if (doMap) {
+                //This may look a little strange, but just reusing eventItem again for efficiency.
+                //The eventItem is now of type TEVENT which was converted to conform to the sender's desired format
+                //before getting serialized
+                eventItem = objectMapper.map(eventItem);
+            }
+
+            Envelope envelope = buildStreamingEnvelope(sequenceId, position, Boolean.toString(isLast));
+            EventContext context = new EventContext(Directions.Out, envelope, eventItem);
+            streamBuffer.add(new EventStreamQueueItem(context));
+
+            log.debug("Buffering event to be sequenced with sequenceId: " + sequenceId + ", position: " + position + ", isLast: " + isLast);
+
+            //Flush bufffer if batch limit has been met
+            if (streamBuffer.size() == batchLimit || isLast) {
+                flushBufferToStream(streamBuffer);
+                streamBuffer.clear();
+            }
+            position++;
+        }
+        log.debug("leave publish to sequence");
+    }
+
+    /**
+     * Publishes messages on the {@link cmf.eventing.IStreamingEventBus} after the numberOfEvents limit has been met.
+     *
+     * @param numberOfEvents
+     */
+    @Override
+    public void setBatchLimit(int numberOfEvents) {
+        if (numberOfEvents <= 0) {
+            log.warn("Message batch limit cannot be less than or equal to zero, using 10 as the default limit.");
+            this.batchLimit = 10;
+        } else {
+            this.batchLimit = numberOfEvents;
+        }
+    }
+
+    private void validateEventStream(Iterator<Object> eventStream) throws Exception {
+        if (null == eventStream) {
+            throw new IllegalArgumentException("Cannot publish a null event stream");
+        }
+    }
+
+    private <TEVENT> boolean isValidMapper(IStreamingMapperCallback<TEVENT> objectMapper) {
+        boolean doMap = true;
+        if (null == objectMapper) {
+            log.warn("No IStreamingMapperCallback supplied. Will not perform transformational mapping of elements in event stream");
+            doMap = false;
+        }
+        return doMap;
+    }
+
+    protected Envelope buildStreamingEnvelope(String sequenceId, int position, String isLast) {
+        Envelope envelope = new Envelope();
+        envelope.setHeader(SEQUENCE_ID, sequenceId);
+        envelope.setHeader(POSITION, position + "");
+        envelope.setHeader(IS_LAST, isLast);
+        return envelope;
+    }
+
+    private void flushBufferToStream(Queue<EventStreamQueueItem> streamQueue) throws Exception {
+        while (streamQueue.size() > 0) {
+            final EventStreamQueueItem queueItem = streamQueue.remove();
+            this.processEvent(queueItem.getEventContext(),
+                    outboundProcessors,
+                    new IContinuationCallback() {
+                        @Override
+                        public void continueProcessing() throws Exception {
+                            envelopeBus.send(queueItem.getEnvelope());
+                        }
+                    });
+            Envelope env = queueItem.getEnvelope();
+            String seqId = env.getHeader(SEQUENCE_ID);
+            String pos = env.getHeader(POSITION);
+            String last = env.getHeader(IS_LAST);
+            log.debug("Sending sequenced event with sequenceId: " + seqId + ", position: " + pos + ", isLast: " + last);
+        }
+    }
 
 }
