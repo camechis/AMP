2fb1e740a0b2aeeead6b58dc7789aa1a31aedd1b
Jared Holmberg <jholmberg@bericotechnologies.com>
2013-06-06 22:26:31 -0400

    Finishing implementation for the StreamingCollectionRegistration to appropriately deliver a Collection when the last event in a sequence is received.

--- java/amp-eventing/src/main/java/amp/eventing/StreamingCollectionRegistration.java
+++ java/amp-eventing/src/main/java/amp/eventing/StreamingCollectionRegistration.java
@@ -5,11 +5,13 @@ import cmf.bus.EnvelopeHeaderConstants;
 import cmf.bus.IEnvelopeFilterPredicate;
 import cmf.bus.IRegistration;
 import cmf.eventing.patterns.streaming.IStreamingCollectionHandler;
+import cmf.eventing.patterns.streaming.IStreamingProgressNotifier;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Specialized {@link cmf.bus.IRegistration} that handles the event by aggregating events from a common sequence
@@ -22,8 +24,11 @@ public class StreamingCollectionRegistration<TEVENT> implements IRegistration {
     protected IEnvelopeFilterPredicate filterPredicate;
     protected IInboundProcessorCallback processorCallback;
     protected Map<String, String> registrationInfo;
-    protected Collection<TEVENT> collectedEvents;
+    protected ConcurrentHashMap<String, Collection<TEVENT>> collectedEvents;
 
+    protected static final String SEQUENCE_ID = "sequenceId";
+    protected static final String POSITION = "position";
+    protected static final String IS_LAST = "isLast";
 
     @Override
     public IEnvelopeFilterPredicate getFilterPredicate() {
@@ -36,12 +41,12 @@ public class StreamingCollectionRegistration<TEVENT> implements IRegistration {
     }
 
     public StreamingCollectionRegistration(IStreamingCollectionHandler<TEVENT> handler, IInboundProcessorCallback processorCallback) {
-        this.eventHandler = eventHandler;
+        this.eventHandler = handler;
         this.processorCallback = processorCallback;
 
         registrationInfo = new HashMap<String, String>();
         registrationInfo.put(EnvelopeHeaderConstants.MESSAGE_TOPIC, eventHandler.getEventType().getCanonicalName());
-        this.collectedEvents = new ArrayList<TEVENT>();
+        this.collectedEvents = new ConcurrentHashMap<String, Collection<TEVENT>>();
     }
 
     @Override
@@ -51,9 +56,23 @@ public class StreamingCollectionRegistration<TEVENT> implements IRegistration {
 
         if (null != event) {
             try {
-                collectedEvents.add(event);
-                //TODO: Determine how to collect the rest of the events
-                result = eventHandler.handle(event, env.getHeaders());
+                String sequenceId = env.getHeader(SEQUENCE_ID);
+                boolean isLast = Boolean.parseBoolean(env.getHeader(IS_LAST));
+
+                if (!collectedEvents.containsKey(sequenceId)) {
+                    collectedEvents.put(sequenceId, new ArrayList<TEVENT>());
+                }
+
+                collectedEvents.get(sequenceId).add(event);
+                IStreamingProgressNotifier notifier = eventHandler.getProgressNotifier();
+                if (null != notifier) {
+                    notifier.updateProgress(sequenceId,
+                            collectedEvents.get(sequenceId).size());
+                }
+
+                if (isLast) {
+                    result = this.eventHandler.handle(collectedEvents.get(sequenceId), env.getHeaders());
+                }
             } catch (Exception ex) {
                 result = handleFailed(env, ex);
             }
@@ -64,6 +83,10 @@ public class StreamingCollectionRegistration<TEVENT> implements IRegistration {
 
     @Override
     public Object handleFailed(Envelope env, Exception ex) throws Exception {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        try {
+            return eventHandler.handleFailed(env, ex);
+        } catch (Exception failedToFail) {
+            throw failedToFail;
+        }
     }
 }
--- java/amp-eventing/src/main/java/amp/eventing/StreamingIteratorRegistration.java
+++ java/amp-eventing/src/main/java/amp/eventing/StreamingIteratorRegistration.java
@@ -1,16 +1,49 @@
 package amp.eventing;
 
+import cmf.bus.Envelope;
+import cmf.bus.IEnvelopeFilterPredicate;
+import cmf.bus.IRegistration;
 import cmf.eventing.IEventHandler;
+import cmf.eventing.patterns.streaming.IStreamingIteratorHandler;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Created with IntelliJ IDEA.
+ * Specialized {@link cmf.bus.IRegistration} that handles the event by placing them in a
+ * {@link java.util.Iterator} from which they can be extracted as they become available.
  * User: jholmberg
  * Date: 6/5/13
- * Time: 7:02 PM
- * To change this template use File | Settings | File Templates.
  */
-public class StreamingIteratorRegistration extends EventRegistration {
-    public StreamingIteratorRegistration(IEventHandler eventHandler, IInboundProcessorCallback processorCallback) {
-        super(eventHandler, processorCallback);
+public class StreamingIteratorRegistration<TEVENT> implements IRegistration {
+    protected IStreamingIteratorHandler<TEVENT> eventHandler;
+    protected IEnvelopeFilterPredicate filterPredicate;
+    protected IInboundProcessorCallback processorCallback;
+    protected  Map<String, String> registrationInfo;
+    protected ConcurrentHashMap<String, Iterator<TEVENT>> eventIterators;
+
+    protected static final String SEQUENCE_ID = "sequenceId";
+    protected static final String POSITION = "position";
+    protected static final String IS_LAST = "isLast";
+
+    @Override
+    public IEnvelopeFilterPredicate getFilterPredicate() {
+        return filterPredicate;
+    }
+
+    @Override
+    public Map<String, String> getRegistrationInfo() {
+        return
+    }
+
+    @Override
+    public Object handle(Envelope env) throws Exception {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Object handleFailed(Envelope env, Exception ex) throws Exception {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 }
