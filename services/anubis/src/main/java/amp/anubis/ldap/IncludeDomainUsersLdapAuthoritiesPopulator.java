46b76c55a72a8c8c74b2b68ee1c920b4d394e697
John Ruiz <jruiz@johnruiz.com>
2013-07-17 17:07:29 -0400

    Added logging to new IncludeDomainUsersLdapAuthoritiesPopulator

--- services/anubis/src/main/java/amp/anubis/ldap/IncludeDomainUsersLdapAuthoritiesPopulator.java
+++ services/anubis/src/main/java/amp/anubis/ldap/IncludeDomainUsersLdapAuthoritiesPopulator.java
@@ -1,5 +1,7 @@
 package amp.anubis.ldap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ldap.core.ContextSource;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,6 +12,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class IncludeDomainUsersLdapAuthoritiesPopulator extends DefaultLdapAuthoritiesPopulator {
+
+    private static final Logger Log = LoggerFactory.getLogger(IncludeDomainUsersLdapAuthoritiesPopulator.class);
 
 
     public IncludeDomainUsersLdapAuthoritiesPopulator(ContextSource contextSource, String groupSearchBase) {
@@ -26,6 +30,7 @@ public class IncludeDomainUsersLdapAuthoritiesPopulator extends DefaultLdapAutho
         String primaryGroup = user.getStringAttribute("primaryGroupID");
 
         if (primaryGroup.equalsIgnoreCase("513")) {
+            Log.info("User '{}' has primary group set to 513, which indicates a domain user. Adding that authority.", username);
             additionalAuthorities.add(new SimpleGrantedAuthority("Domain Users"));
         }
 
