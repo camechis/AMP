0da6a03d5c096319674533cdec6b0910c8ae42e2
John Ruiz <jruiz@johnruiz.com>
2013-07-18 12:12:15 -0400

    added more unit tests around token verification

--- services/anubis/src/test/java/amp/anubis/tests/DefaultTokenManagerTests.java
+++ services/anubis/src/test/java/amp/anubis/tests/DefaultTokenManagerTests.java
@@ -144,7 +144,6 @@ public class DefaultTokenManagerTests {
             fail("Should have thrown an IllegalArgumentException but didn't.");
         }
 
-
         @Test(expected = IllegalArgumentException.class)
         public void should_throw_IllegalArgumentException_if_token_has_null_token() {
 
@@ -171,6 +170,39 @@ public class DefaultTokenManagerTests {
 
             // assert
             fail("Should have thrown an IllegalArgumentException but didn't.");
+        }
+
+        @Test
+        public void random_password_should_not_verify() {
+
+            // create
+            DefaultTokenManager tokenManager = new DefaultTokenManager();
+            NamedToken badToken = new NamedToken("testUser", "random password");
+
+            // test
+            boolean shouldBeFalse = tokenManager.verifyToken(badToken);
+
+            // assert
+            assertFalse(shouldBeFalse);
+        }
+
+        @Test
+        public void correct_password_wrong_username_should_not_verify() {
+
+            // create
+            DefaultTokenManager tokenManager = new DefaultTokenManager();
+            UserDetails mockUser = mock(UserDetails.class);
+
+            // setup
+            when(mockUser.getUsername()).thenReturn("testUser");
+            NamedToken token = tokenManager.generateToken(mockUser);
+            NamedToken changedToken = new NamedToken("differentUser", token.getIdentity());
+
+            // test
+            boolean shouldBeFalse = tokenManager.verifyToken(changedToken);
+
+            // assert
+            assertFalse(shouldBeFalse);
         }
     }
 
