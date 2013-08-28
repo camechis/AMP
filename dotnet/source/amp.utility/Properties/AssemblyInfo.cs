b035ae0d7dbd193f9557fc88e38fb6b32c45c403
Kenneth Baltrinic <kenneth@baltrinic.com>
2013-08-28 13:17:45 -0400

    Created empty amp.utility project.

---
+++ dotnet/source/amp.utility/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Local Descriptive Stuff
+[assembly: AssemblyTitle("AMP implementation of CMF Envelope Bus")]
+[assembly: AssemblyDescription("Contains the AMP implementation of the envelope bus.")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9f0fa635-6d6a-4df4-84fc-6065a9d244d2")]
