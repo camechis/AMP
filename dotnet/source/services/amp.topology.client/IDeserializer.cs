867446123f3419fc1cc3fbba4de2a69ce51b729a
Kenneth Baltrinic <kenneth@baltrinic.com>
2013-08-29 10:30:29 -0400

    Genericizes IRoutingInfoDeserializer to IDeserializer<T> ahead of moving it into Utilities.

---
+++ dotnet/source/services/amp.topology.client/IDeserializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+
+namespace amp.topology.client
+{
+    public interface IDeserializer<T> : IDisposable
+    {
+        T Deserialize(byte[] routingBytes);
+
+        T Deserialize(Stream routingStream);
+    }
+}
---
+++ dotnet/source/services/amp.topology.client/Utf8JsonDeserializer.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Text;
+
+using log4net;
+using Newtonsoft.Json;
+
+
+namespace amp.topology.client
+{
+    public class Utf8JsonDeserializer<T> : IDeserializer<T> 
+    {
+        protected static readonly ILog Log = LogManager.GetLogger(typeof(Utf8JsonDeserializer<T>));
+
+
+        public T Deserialize(byte[] routingBytes)
+        {
+            T routing;
+
+            using (MemoryStream buffer = new MemoryStream(routingBytes))
+            {
+                routing = this.DeserializeRouting(buffer);
+                buffer.Close();
+            }
+
+            return routing;
+        }
+
+        public T Deserialize(Stream routingStream)
+        {
+            T routing;
+
+            using (MemoryStream buffer = new MemoryStream())
+            {
+                // using CopyTo because of a stackoverflow answer
+                // http://stackoverflow.com/questions/221925/creating-a-byte-array-from-a-stream
+                routingStream.CopyTo(buffer);
+                routing = this.DeserializeRouting(buffer);
+                buffer.Close();
+            }
+
+            return routing;
+        }
+
+        public T DeserializeRouting(MemoryStream buffer)
+        {
+            // convert the buffer into an UTF-8 encoded string
+            string jsonUtf8 = Encoding.UTF8.GetString(buffer.ToArray());
+
+            // log the string for debugging
+            Log.DebugFormat("Deserializing the following string into RoutingInfo: {0}", jsonUtf8);
+
+            // use Json.NET to deserialize the JSON string
+            return JsonConvert.DeserializeObject<T>(jsonUtf8);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
