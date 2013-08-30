49f5cb60345823d09bfc41edb82bc2a5c2d812ec
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-08-30 10:03:26 -0400

    Cleaned up using statements (amp.example solution).

--- dotnet/examples/amp.examples.gui.duplex/DuplexEventForm.cs
+++ dotnet/examples/amp.examples.gui.duplex/DuplexEventForm.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
 using Common.Logging;
-
-using cmf.eventing;
 using cmf.eventing.patterns.rpc;
 using amp.examples.gui.messages;
 
