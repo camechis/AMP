f3c212ea8379ed0239bb70c580ea94c9e8d024fa
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-09-03 14:12:05 -0400

    Adding .gitattributes file that will force all text files in the dotnet folder to user CRLF line endings when checked out.
    Remainder of files in this commit are those whose line endings were affected by this change.

--- dotnet/source/GlobalAssemblyInfo.cs
+++ dotnet/source/GlobalAssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // Global Descriptive Stuff
 [assembly: AssemblyProduct("Advanced Modular Platform")]
