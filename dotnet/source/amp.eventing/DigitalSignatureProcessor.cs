6067ba8276563f3433ad53bcbcb3c0c2dd6d7a63
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-09-09 11:29:53 -0400

    Updated DigitalSignalProcessor to be implement IMessageProcessor instead of now defunct IEventProcessor.

--- dotnet/source/amp.eventing/DigitalSignatureProcessor.cs
+++ dotnet/source/amp.eventing/DigitalSignatureProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using amp.messaging;
 using Common.Logging;
 
 using cmf.bus;
@@ -8,7 +9,7 @@ using amp.bus.security;
 
 namespace amp.eventing
 {
-    public class DigitalSignatureProcessor : IEventProcessor
+    public class DigitalSignatureProcessor : IMessageProcessor
     {
         protected ICertificateProvider _certProvider;
         protected X509Certificate2 _cert;
@@ -30,19 +31,19 @@ namespace amp.eventing
         }
 
 
-        public void ProcessEvent(EventContext context, Action continueProcessing)
+        public void ProcessMessage(MessageContext context, Action continueProcessing)
         {
-            if (EventContext.Directions.In == context.Direction)
+            if (MessageContext.Directions.In == context.Direction)
             {
                 this.ProcessInbound(context, continueProcessing);
             }
-            if (EventContext.Directions.Out == context.Direction)
+            if (MessageContext.Directions.Out == context.Direction)
             {
                 this.ProcessOutbound(context, continueProcessing);
             }
         }
 
-        public void ProcessInbound(EventContext context, Action continueProcessing)
+        public void ProcessInbound(MessageContext context, Action continueProcessing)
         {
             bool signatureVerified = false;
             Envelope env = context.Envelope;
@@ -82,7 +83,7 @@ namespace amp.eventing
             if (signatureVerified) { continueProcessing(); }
         }
 
-        public void ProcessOutbound(EventContext context, Action continueProcessing)
+        public void ProcessOutbound(MessageContext context, Action continueProcessing)
         {
             if (null == _cert) { return; }
 
@@ -101,6 +102,11 @@ namespace amp.eventing
                 _log.Error("Failed to digitally sign the event", ex);
                 throw;
             }
+        }
+
+        public void Dispose()
+        {
+            //Nothing to do.
         }
     }
 }
