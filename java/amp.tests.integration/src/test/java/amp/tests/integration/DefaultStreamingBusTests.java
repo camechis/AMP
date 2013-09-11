fc411b38d64b252f5c029f770a399e91bbdf93c2
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-09-10 23:09:33 -0400

    Implements java integration tests for DefaultStreamingEventBus.

---
+++ java/amp.tests.integration/src/test/java/amp/tests/integration/DefaultStreamingBusTests.java
@@ -0,0 +1,161 @@
+package amp.tests.integration;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+import cmf.eventing.patterns.streaming.IStreamingCollectionHandler;
+import cmf.eventing.patterns.streaming.IStreamingEventBus;
+import cmf.eventing.patterns.streaming.IStreamingReaderHandler;
+import cmf.eventing.patterns.streaming.IEventStream;
+import cmf.eventing.patterns.streaming.StreamingEventItem;
+
+public class DefaultStreamingBusTests extends DefaultEventBusTests {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultStreamingBusTests.class);
+
+	private static IStreamingEventBus streamingBus;
+
+	@BeforeClass
+	public static void BeforeAllTests(){
+		DefaultEventBusTests.BeforeAllTests();
+		DefaultEventBusTests.bus = streamingBus = (IStreamingEventBus) context.getBean("streamingEventBus");
+	}
+	
+	@AfterClass
+	public static void AfterAllTests(){
+		DefaultEventBusTests.AfterAllTests();
+	}
+	
+    @Test
+    public void Should_receive_all_segments_published_as_a_chunked_sequence() throws Exception
+    {
+    	CountDownLatch signal = new CountDownLatch(1);
+        CollectionHandler handler = new CollectionHandler(signal);
+        streamingBus.subscribeToCollection(handler);
+
+
+        ArrayList<TestStreamEvent> eventSequence = new ArrayList<TestStreamEvent>();
+        eventSequence.add(new TestStreamEvent(1));
+        eventSequence.add(new TestStreamEvent(2));
+        eventSequence.add(new TestStreamEvent(3));
+
+        streamingBus.publishChunkedSequence(eventSequence);
+
+        signal.await(10, TimeUnit.SECONDS);
+
+        assertNotNull("The collection of events was not receieved before the timeout expired.", handler.ReceivedEvents);
+        assertEquals(3,handler.ReceivedEvents.length);
+        for(int i = 1; i <=3; i++){
+        	assertEquals(i, handler.ReceivedEvents[i=1].getEvent().Sequence);
+        }
+    }
+
+    @Test
+    public void Should_receive_all_events_published_as_a_stream() throws Exception
+    {
+    	CountDownLatch signal = new CountDownLatch(1);
+        ReaderHandler handler = new ReaderHandler(signal);
+
+        streamingBus.subscribeToReader(handler); 
+        
+        ArrayList<TestStreamEvent> streamEvents = new ArrayList<TestStreamEvent>();
+        streamEvents.add(new TestStreamEvent(1));
+        streamEvents.add(new TestStreamEvent(2));
+        streamEvents.add(new TestStreamEvent(3));
+
+        IEventStream stream = streamingBus.createStream(TestStreamEvent.class.getCanonicalName());
+        try {
+            stream.setBatchLimit(2);
+
+            for(TestStreamEvent event : streamEvents)
+            {
+                stream.publish(event);
+            }
+        } finally {
+        	stream.dispose();
+        }
+
+        signal.await(10, TimeUnit.SECONDS);
+
+        assertNotNull("The collection of events was not receieved before the timeout expired.", handler.ReceivedEvents);
+        assertEquals(3,handler.ReceivedEvents.length);
+        for(int i = 1; i <=3; i++){
+        	assertEquals(i, handler.ReceivedEvents[i=1].getEvent().Sequence);
+        }
+    }
+
+    public class CollectionHandler implements IStreamingCollectionHandler<TestStreamEvent> {
+        private final CountDownLatch _signal;
+        public StreamingEventItem<TestStreamEvent>[] ReceivedEvents;
+
+        public CollectionHandler(CountDownLatch signal) {
+            _signal = signal;
+        }
+
+		@Override
+		public Class<TestStreamEvent> getEventType() {
+			return TestStreamEvent.class;
+		}
+
+		@Override
+		public void handleCollection( Collection<StreamingEventItem<TestStreamEvent>> events) {
+            ReceivedEvents = events.toArray(ReceivedEvents);
+            _signal.countDown();
+
+            LOG.debug(String.format("Received a collection from AMPere of size: %d", events.size()));
+
+            for(StreamingEventItem<TestStreamEvent> eventItem : events) {
+            	LOG.debug(String.format("Event Item: SequenceId => %d, Position => %d, Content => %s",
+                    eventItem.getSequenceId(), eventItem.getPosition(), eventItem.getEvent().toString()));
+            }
+
+            LOG.debug("Processing complete.");
+		}
+
+		@Override
+		public void onPercentCollectionReceived(double percent) {
+        	LOG.debug(String.format("Percent of events received: %d%", percent));
+		}
+    }
+
+    public class ReaderHandler implements IStreamingReaderHandler<TestStreamEvent> {
+        private final CountDownLatch _signal;
+        private final ArrayList<StreamingEventItem<TestStreamEvent>> _receivedEvents = new ArrayList<StreamingEventItem<TestStreamEvent>>();
+
+        public StreamingEventItem<TestStreamEvent>[] ReceivedEvents;
+
+
+        public ReaderHandler(CountDownLatch signal) {
+            _signal = signal;
+        }
+
+		@Override
+		public void dispose() {
+			 LOG.debug("Notified that last event has been processed for the stream.");
+	         _signal.countDown();
+		}
+
+		@Override
+		public Class<TestStreamEvent> getEventType() {
+			return TestStreamEvent.class;
+		}
+
+		@Override
+		public void onEventRead(StreamingEventItem<TestStreamEvent> eventItem) {
+            _receivedEvents.add(eventItem);
+            LOG.debug(String.format("Message received: (sequenceId: %d), (position: %d), Event Value: %s",
+            		eventItem.getSequenceId(), eventItem.getPosition(), eventItem.getEvent().toString()));
+            
+		}
+    }
+}
