d8b0a5bf1c57e22495fd942752c09da0c08bb445
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-09-28 22:44:08 -0400

    Drying out test code.

--- dotnet/tests/amp.tests/rabbit/connection/ConnectionManagementTests.cs
+++ dotnet/tests/amp.tests/rabbit/connection/ConnectionManagementTests.cs
@@ -77,12 +77,7 @@ namespace amp.tests.rabbit.connection
         [Test]
         public void Register_should_create_a_connection_and_a_channel()
         {
-            var env = new Envelope();
-            env.Headers.Add(EnvelopeHeaderConstants.MESSAGE_TOPIC, "testing");
-
-            _transport.Register(new MessageRegistration( e => null , new NullHandler()));
-
-            _modelCreateSignal.WaitOne(250); //Registration happens on a background thread.
+            RegisterNullHandler();
 
             _rmqFactory.Verify(r => r.CreateConnection(), "Connection to RabbitMQ was not created.");
             _connections[0].Verify(c => c.CreateModel(), "Channel for listening was not created.");
@@ -91,18 +86,9 @@ namespace amp.tests.rabbit.connection
         [Test]
         public void Register_should_restart_on_new_channel_if_channel_fails()
         {
-            var env = new Envelope();
-            env.Headers.Add(EnvelopeHeaderConstants.MESSAGE_TOPIC, "testing");
+            RegisterNullHandler();
 
-            _transport.Register(new MessageRegistration(e => null, new NullHandler()));
-
-            _modelCreateSignal.WaitOne(250); //Registration happens on a background thread.
-
-            _modelCreateSignal.Reset();
-
-            _models[0].Raise(m => m.ModelShutdown += null, new ShutdownEventArgs(ShutdownInitiator.Peer, 0, "Testing..."));
-
-            _modelCreateSignal.WaitOne(250); //Registration happens on a background thread.
+            SimulateChannelClosure(ShutdownInitiator.Peer);
 
             _rmqFactory.Verify(r => r.CreateConnection(), Times.Once(), "Connection to RabbitMQ incorrectly recreated.");
             _connections[0].Verify(c => c.CreateModel(), Times.Exactly(2), "Channel for listening was not recreated.");
@@ -111,21 +97,31 @@ namespace amp.tests.rabbit.connection
         [Test]
         public void Register_should_not_restart_on_new_channel_is_closed_by_application()
         {
+            RegisterNullHandler();
+
+            SimulateChannelClosure(ShutdownInitiator.Application);
+
+            _rmqFactory.Verify(r => r.CreateConnection(), Times.Once(), "Connection to RabbitMQ incorrectly recreated.");
+            _connections[0].Verify(c => c.CreateModel(), Times.Once(), "Channel for listening was recreated.");
+        }
+
+        private void RegisterNullHandler()
+        {
             var env = new Envelope();
             env.Headers.Add(EnvelopeHeaderConstants.MESSAGE_TOPIC, "testing");
 
             _transport.Register(new MessageRegistration(e => null, new NullHandler()));
 
             _modelCreateSignal.WaitOne(250); //Registration happens on a background thread.
+        }
 
+        private void SimulateChannelClosure(ShutdownInitiator initiator)
+        {
             _modelCreateSignal.Reset();
 
-            _models[0].Raise(m => m.ModelShutdown += null, new ShutdownEventArgs(ShutdownInitiator.Application, 0, "Testing..."));
+            _models[0].Raise(m => m.ModelShutdown += null, new ShutdownEventArgs(initiator, 0, "Testing..."));
 
             _modelCreateSignal.WaitOne(250); //Registration happens on a background thread.
-
-            _rmqFactory.Verify(r => r.CreateConnection(), Times.Once(), "Connection to RabbitMQ incorrectly recreated.");
-            _connections[0].Verify(c => c.CreateModel(), Times.Once(), "Channel for listening was recreated.");
         }
 
         private class TestConnectionFactory : BaseConnectionFactory
