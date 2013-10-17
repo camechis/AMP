8e08343b30adbd0b94aa3dc627c4d4e42f071cda
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-10-17 10:53:00 -0400

    Fixes misconfiguration of AnubisOnly connection scenario test to truly be Anubis only.

--- java/amp.tests.integration/src/test/java/amp/tests/integration/configScenarios/AnubisOnlyTests.java
+++ java/amp.tests.integration/src/test/java/amp/tests/integration/configScenarios/AnubisOnlyTests.java
@@ -33,8 +33,8 @@ public class AnubisOnlyTests {
 	public static String[] getConfigFiles(){
 		return new String[]{
 				Bus.All, 
-				Authorization.AnubisOneWaySsl,
-				Topology.GtsSSL};
+				Authorization.AnubisBasic,
+				Topology.Simple};
 	}
 	
 	@BeforeClass
