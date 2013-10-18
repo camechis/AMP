ce15b2a781353996ba849bd8fb7f8962bccc3dd8
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-10-18 14:46:35 -0400

    Adding some tests to assert the intended behavior of BaseRoute equality and hash code as these are not obviously correctly implemented.

---
+++ java/amp-rabbit/src/test/java/amp/rabbit/topology/BaseRouteTests.java
@@ -0,0 +1,82 @@
+package amp.rabbit.topology;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class BaseRouteTests {
+
+	BaseRoute referenceModel = new Route();
+	BaseRoute equivelentModel = new Route();
+	BaseRoute brokerNotEquivelentModel = new Route();
+	BaseRoute exchangeNotEquivelentModel = new Route();
+	BaseRoute routeEquivelentModel = new Route();
+	BaseRoute routeNotEquivelentModel = new Route();
+	
+	@Before
+	public void setup(){
+		
+		referenceModel.brokers.add(new Broker(null,"reference", 0, true));
+		referenceModel.exchange = new Exchange("reference", "direct", true, true, true, null, null);
+		referenceModel.routingKeys.add("route");
+
+		equivelentModel.brokers.add(new Broker(null,"reference", 0, true));
+		equivelentModel.exchange = new Exchange("reference", "direct", true, true, true, null, null);
+		equivelentModel.routingKeys.add("route");
+
+		brokerNotEquivelentModel.brokers.add(new Broker(null,"reference", 0, false));
+		brokerNotEquivelentModel.exchange = new Exchange("reference", "direct", true, true, true, null, null);
+		brokerNotEquivelentModel.routingKeys.add("route");
+
+		exchangeNotEquivelentModel.brokers.add(new Broker(null,"reference", 0, true));
+		exchangeNotEquivelentModel.exchange = new Exchange("other", "direct", true, true, true, null, null);
+		exchangeNotEquivelentModel.routingKeys.add("route");
+
+		routeNotEquivelentModel.routingKeys.add("other-route");
+		routeNotEquivelentModel.exchange = new Exchange("reference", "direct", true, true, true, null, null);
+		routeNotEquivelentModel.brokers.add(new Broker(null,"reference", 0, true));
+	}
+
+	@Test
+	public void Equals_should_return_true_for_equivelent_routes(){
+		assertTrue(referenceModel.equals(equivelentModel));
+	}
+	
+	@Test
+	public void Equals_should_return_false_if_broker_values_differ(){
+		assertFalse(referenceModel.equals(brokerNotEquivelentModel));
+	}
+	
+	@Test
+	public void Equals_should_return_false_if_exchange_values_differ(){
+		assertFalse(referenceModel.equals(exchangeNotEquivelentModel));
+	}
+	
+	@Test
+	public void Equals_should_return_false_if_route_values_differ(){
+		assertFalse(referenceModel.equals(routeNotEquivelentModel));
+	}
+	
+	@Test
+	public void Hashcodes_should_not_differ_for_equivelent_routes(){
+		assertEquals(referenceModel.hashCode() , equivelentModel.hashCode());
+	}
+
+	@Test
+	public void Hashcodes_should_differ_if_broker_entries_differ(){
+		assertNotEquals(referenceModel.hashCode() , brokerNotEquivelentModel.hashCode());
+	}
+	
+	@Test
+	public void Hashcodes_should_differ_if_exchanges_differ(){
+		assertNotEquals(referenceModel.hashCode() , exchangeNotEquivelentModel.hashCode());
+	}
+	
+	@Test
+	public void Hashcodes_should_differ_if_routingKey_entries_differ(){
+		assertNotEquals(referenceModel.hashCode() , routeNotEquivelentModel.hashCode());
+	}
+	
+	private static class Route extends BaseRoute {
+	}
+}
