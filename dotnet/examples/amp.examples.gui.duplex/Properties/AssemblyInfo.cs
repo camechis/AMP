8c047a6815a968936eeb04a7900c93f60be8f07c
Ken Baltrinic <kbaltrinic@bericotechnologies.com>
2013-10-29 09:14:29 -0400

    Fixes issue introduced when 3.2.1 release branch was merged back into develop. Namely all 3.3.0 version numbers on the .net side reverted to 3.2.1.  Some similar issues also occured on the java side and are also fixed herein.
    Exception.  Updating the GTS service version to 3.3.0-SNAPSHOT break its build severly because it uses ${project.version} as its AMP version.  Thus we still need to update the GTS to use the 3.3.0-SNAPSHOT versions of AMP and CMF.  But that will take some work.

--- dotnet/examples/amp.examples.gui.duplex/Properties/AssemblyInfo.cs
+++ dotnet/examples/amp.examples.gui.duplex/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("amp.examples.duplex")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("amp.examples.duplex")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,15 +15,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e97b4805-82e6-400c-9cd8-d0ed2b2b1fd4")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
--- dotnet/examples/amp.examples.gui.messages/Properties/AssemblyInfo.cs
+++ dotnet/examples/amp.examples.gui.messages/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("amp.examples.messages")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("amp.examples.messages")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a7d917c8-5019-4c56-a415-326e08c7e96c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.1.0")]
-[assembly: AssemblyFileVersion("3.2.1.0")]
