3dc34e74e54665e3554d613f3f9ca6dc5a2c450c
Travis Camechis <camechis@gmail.com>
2013-12-11 18:32:56 -0500

    expermintation

--- java/amp-spring/src/test/java/amp/spring/ConfigurationTest.java
+++ java/amp-spring/src/test/java/amp/spring/ConfigurationTest.java
@@ -3,13 +3,24 @@ package amp.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import amp.spring.configuration.AmpereConfig;
+import amp.spring.configuration.CommandableRoutingInfoCache;
+import amp.spring.configuration.GlobalTopologyConfig;
+import amp.spring.configuration.SSLHttpClientProviderConfig;
+import amp.spring.configuration.TokenAmpereConnectionConfig;
+
 public class ConfigurationTest {
 
-	public static void main( String[] args ) {
-		
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(MyConfig.class);
-		
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(TokenAmpereConnectionConfig.class);
+		ctx.register(CommandableRoutingInfoCache.class);
+		ctx.register(GlobalTopologyConfig.class);
+		ctx.register(SSLHttpClientProviderConfig.class);
+	    ctx.register(AmpereConfig.class);
+		ctx.refresh();
+
 	}
-	
-	
+
 }
